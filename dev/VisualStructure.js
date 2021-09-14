eda4b7f639ae941e5d3d7d92f49f0cbcf15df1ff
Reider745 <70357814+Reider745@users.noreply.github.com>
2021-09-14 20:53:12 +0300

    Update 2.1
    - added VisualStructure prototype

--- dev/VisualStructure.js
+++ dev/VisualStructure.js
@@ -1,10 +1,10 @@
 let VisualStructure = {
-	getArrMesh(name, size){
+	getArrMesh(name, size, value){
 		let BaseArr = [];
 		let pos = [];
 		let stru = Structure.getStructure(name||[]);
 		for(let i in stru){
-			if(stru[i].state.id == 0)
+			if(stru[i].state.id == 0 || value)
 				continue;
 			let base = new Animation.Item(stru[i].x, stru[i].y, stru[i].z);
 			base.describeItem({
@@ -13,6 +13,8 @@ let VisualStructure = {
 				size: size || .95,
 				material: "visual_structure"
 			});
+			base.block = {};
+			base.block.id = stru[i].state.id;
 			BaseArr.push(base);
 			pos.push([stru[i].x, stru[i].y, stru[i].z]);
 			if(stru[i].stateExtra.id == 0)
@@ -29,32 +31,43 @@ let VisualStructure = {
 		}
 		return {base: BaseArr, pos: pos};
 	},
-	Animation(stru, size){
-		let BaseArr = VisualStructure.getArrMesh(stru, size);
+	Animation(stru, size, value){
+		let BaseArr = VisualStructure.getArrMesh(stru, size, value);
 		
 		this.loaded = false;
-		this.setStructure = function(stru, size){
+		this.setStructure = function(stru, size, value){
 			this.destroy();
-			BaseArr = VisualStructure.getArrMesh(stru, size);
+			BaseArr = VisualStructure.getArrMesh(stru, size, value);
 		}
 		this.getStructure = function(){
 			return stru;
+		}
+		let prot = {
+			isLoad(){return true},
+			load(){return "visual_structure"},
+			tick(){}
+		};
+		this.setPrototype = function(obj){
+			obj.isLoad = obj.isLoad || function(){return true}
+			obj.load = obj.load || function(){return "visual_structure"}
+			obj.tick = obj.tick || function(){}
+			prot = obj;
+		}
+		this.getPrototype = function(obj){
+			return prot;
 		}
 		this.load = function(x, y, z, a){
 			this.loaded = true;
 			for(let i in BaseArr.base){
 				let pos = BaseArr.pos[i];
 				BaseArr.base[i].setPos(x+pos[0],y+pos[1],z+pos[2]);
-				BaseArr.base[i].load();
-				BaseArr.base[i].getShaderUniforms().setUniformValue("visual_structure", "A", a || .6);
-			}
-		}
-		this.loadCustom = function(x, y, z, func, a){
-			this.loaded = true;
-			for(let i in BaseArr.base){
-				let pos = BaseArr.pos[i];
-				BaseArr.base[i].setPos(x+pos[0],y+pos[1],z+pos[2]);
-				BaseArr.base[i].loadCustom(func);
+				if(prot.isLoad(x+pos[0],y+pos[1],z+pos[2], {x: pos[0], y: pos[1], z: pos[2]}, BaseArr.base[i]))
+					BaseArr.base[i].loadCustom(function(){
+						prot.tick(x+pos[0],y+pos[1],z+pos[2], {x: pos[0], y: pos[1], z: pos[2]}, this)
+					});
+				let material = prot.load(x+pos[0],y+pos[1],z+pos[2], {x: pos[0], y: pos[1], z: pos[2]}, BaseArr.base[i])
+				BaseArr.base[i].render.setMaterial(material)
+				
 				BaseArr.base[i].getShaderUniforms().setUniformValue("visual_structure", "A", a || .6);
 			}
 		}
