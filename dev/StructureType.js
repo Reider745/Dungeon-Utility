d432e3d8ee14aa131f34e115fe498cbe3c7e6bd7
Reider745 <70357814+Reider745@users.noreply.github.com>
2021-09-18 18:07:51 +0300

    Update
    Optimization

--- dev/StructureType.js
+++ dev/StructureType.js
@@ -15,7 +15,7 @@ StructureLoader.registerType("DungeonAPI", {
 		let stru = file.split(":");
 		for(let i in stru){
 			let data = stru[i].split(".");
-			arr.push(new BlockData(parseInt(data[2]), parseInt(data[3]), parseInt(data[4]), new BlockState(Block.convertBlockToItemId(parseInt(data[0]) ? parseInt(data[0]) : BlockID[data[0]]), parseInt(data[1])), new BlockState(0, {}), new NBT.CompoundTag()))
+			arr.push(new BlockData(parseInt(data[2]), parseInt(data[3]), parseInt(data[4]), new BlockState(Block.convertBlockToItemId(parseInt(data[0]) ? parseInt(data[0]) : BlockID[data[0]]), parseInt(data[1]))))
 		}
 		return arr;
 	},
@@ -36,7 +36,7 @@ StructureLoader.registerType("DungeonAPI_V2", {
 		let stru = JSON.parse(file);
 		for(let i in stru){
 			let data = stru[i].split(".");
-			arr.push(new BlockData(parseInt(data[2]), parseInt(data[3]), parseInt(data[4]), new BlockState(parseInt(data[0]) ? parseInt(data[0]) : BlockID[data[0]], parseInt(data[1])), new BlockState(0, {}), new NBT.CompoundTag()))
+			arr.push(new BlockData(parseInt(data[2]), parseInt(data[3]), parseInt(data[4]), new BlockState(parseInt(data[0]) ? parseInt(data[0]) : BlockID[data[0]], parseInt(data[1]))))
 		}
 		return arr;
 	},
@@ -57,7 +57,7 @@ StructureLoader.registerType("DungeonCore", {
 			stru[i][3] = stru[i][3] || [];
 			stru[i][3][0] = stru[i][3][0] || 0;
 			stru[i][3][1] = stru[i][3][1] || {};
-			arr.push(new BlockData(parseInt(stru[i][1].split(".")[1]), parseInt(stru[i][1].split(".")[2]), parseInt(stru[i][1].split(".")[3]), new BlockState(typeof(stru[i][0]) == "string" ? BlockID[stru[i][0]] : stru[i][0], stru[i][2]), new BlockState(stru[i][3][0] == "string" ? BlockID[stru[i][3][0]] : stru[i][3][0], stru[i][3][1]), new NBT.CompoundTag()))
+			arr.push(new BlockData(parseInt(stru[i][1].split(".")[1]), parseInt(stru[i][1].split(".")[2]), parseInt(stru[i][1].split(".")[3]), new BlockState(typeof(stru[i][0]) == "string" ? BlockID[stru[i][0]] : stru[i][0], stru[i][2]), new BlockState(stru[i][3][0] == "string" ? BlockID[stru[i][3][0]] : stru[i][3][0], stru[i][3][1]), null))
 		}
 		return arr;
 	},
@@ -76,7 +76,7 @@ StructureLoader.registerType("Structures", {
 		let arr = [];
 		let stru = JSON.parse(file).structure;
 		for(let i in stru){
-			arr.push(new BlockData(stru[i][0], stru[i][1], stru[i][2], new BlockState(typeof(stru[i][3]) == "string" ? BlockID[stru[i][3]] : (typeof(stru[i][3]) == "object" ? stru[i][3].id : 0), stru[i][3].id ? stru[i][3].data : 0), new BlockState(0, {}), new NBT.CompoundTag()));
+			arr.push(new BlockData(stru[i][0], stru[i][1], stru[i][2], new BlockState(typeof(stru[i][3]) == "string" ? BlockID[stru[i][3]] : (typeof(stru[i][3]) == "object" ? stru[i][3].id : 0), stru[i][3].id ? stru[i][3].data : 0)));
 		}
 		return arr;
 	},
@@ -98,7 +98,7 @@ StructureLoader.registerType("DungeonUtility", {
 		let stru = JSON.parse(file);
 		for(let i in stru){
 			let data = stru[i][0].split(".")
-			arr.push(new BlockData(parseInt(data[2])||0, parseInt(data[3])||0, parseInt(data[4])||0, new BlockState(typeof(parseInt(data[0])||(data[0]==""?0:data[0])) == "number" ? parseInt(data[0])||0 : BlockID[data[0]], stru[i][1] || {}), new BlockState(typeof(parseInt(data[1])||(data[1]==""?0:data[1])) == "number" ? parseInt(data[1])||0 : BlockID[data[1]], stru[i][2] || {}), new NBT.CompoundTag()))
+			arr.push(new BlockData(parseInt(data[2])||0, parseInt(data[3])||0, parseInt(data[4])||0, new BlockState(typeof(parseInt(data[0])||(data[0]==""?0:data[0])) == "number" ? parseInt(data[0])||0 : BlockID[data[0]], stru[i][1] || {}), new BlockState(typeof(parseInt(data[1])||(data[1]==""?0:data[1])) == "number" ? parseInt(data[1])||0 : BlockID[data[1]], stru[i][2] || {}), null))
 		}
 		return arr;
 	},
